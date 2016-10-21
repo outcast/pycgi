c9f597ca1170b38141f877b10853ed7b22c51097
James Jones <jajones3@cisco.com>
2016-10-21 00:29:54 -0400

    corrected example.py

--- example.py
+++ example.py
@@ -2,30 +2,32 @@
 
 activate_this = "/venv/bin/activate_this.py"
 execfile(activate_this, dict(__file__=activate_this))
+
 import os
 from pycgi.handler import CGIHTTPRequestHandler
 
 class MainHandler(CGIHTTPRequestHandler):
 
     def do_GET(self):
-        message = "----Headers-----\n"
+        self.do_handle()
+
+    def do_POST(self):
+        self.do_handle()
+
+    def do_handle(self):
+
+        message = "----Headers-----\n\n"
         for header in self.headers:
-            message += header+"=>"+self.headers.get(header)
-        message += "\n-----ENV-----\n"
+            message += header+"=>"+self.headers.get(header)+"\n"
+        message += "\n-----ENV-----\n\n"
         for x in os.environ:
-            message += x+"=>"+os.getenv(x)
-        message += "\n-----BODY-----\n"
-        message += self.rfile.read()
+            message += x+"=>"+os.getenv(x)+"\n"
+        message += "\n-----BODY-----\n\n"
+        message += self.rfile.read()+"\n"
 
         self.send_response(200)
         self.send_header("Content-Type","text/plain")
         self.end_headers()
         self.wfile.write(message)
 
-handler = MainHandler(None,os.getenv("REMOTE_ADDR"),None)
-
-def main():
-    handler.handle()
-
-if __name__ == "__main__":
-    main()
+MainHandler(None,os.getenv("REMOTE_ADDR"),None)
