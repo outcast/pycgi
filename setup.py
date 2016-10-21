0e2cfa6a3af1927f48845c4551acbac032e10831
James Jones <jajones3@cisco.com>
2016-10-21 00:30:49 -0400

    corrected example.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='PyCGI',
-      version='0.1a',
+      version='0.2',
       description='Python CGI handler for Apache',
       author='James Jones',
       author_email='james.voip@gmail.com',
