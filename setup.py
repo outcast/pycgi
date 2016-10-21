774a931dd5b54aed4584994d13cd577b0afcccd2
James Jones <jajones3@cisco.com>
2016-10-20 22:38:38 -0400

    inital commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyCGI',
+      version='0.1a',
+      description='Python CGI handler for Apache',
+      author='James Jones',
+      author_email='james.voip@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+      )
